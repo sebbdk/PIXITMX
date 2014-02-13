afc388d6d12ae0fb0cb212c0c3f1a64a1fbdf536
kasper jensen <sebb@sebb.dk>
2014-02-13 22:17:39 +0100

    initial push

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: kasperjensen
+* @Date:   2014-02-13 22:10:36
+* @Last Modified by:   kasperjensen
+* @Last Modified time: 2014-02-13 22:11:02
+*/
+
+requirejs([], function() {
+	console.log("Hello world");
+});
